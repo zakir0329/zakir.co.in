6cbd00eeae323a9906aa539c7cd70babda377c0f
zakir0329 <zakir032905@gmail.com>
2020-12-09 22:41:22 +0530

    thi is my first web site
    wow

---
+++ javascript.js
@@ -0,0 +1,13 @@
+        function f1()
+        {
+            var x=document.getElementsByTagName("p")
+            x[0].style.backgroundColor="skyblue"
+
+        }
+        function f2()
+        {
+            var x=document.getElementsByTagName("p");
+            x[0].style.backgroundColor="gray"
+        }
+    
+    
